316aabacf3909d84c309df346e4fa66380cf4b0d
Kipotuzer26 <68398517+Kipotuzer26@users.noreply.github.com>
2023-11-08 21:54:51 +0100

    up

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express')
 const http = require('http');
 const { arrayBuffer } = require('stream/consumers');
@@ -10,6 +9,8 @@ const path = require('path');
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server })
+
+// const clients = new Map();
 
 var slide = 1;
 //serve a simple HTML page
@@ -47,7 +48,13 @@ app.get('/slideinfo/', (req, res) => {
     // console.log(path.extname(findFile(req.params.presid, req.params.slide)));
     preslength = 0;
     getDirectories(__dirname + "/cdn/").forEach((directory)=>{
-        preslength = Math.max(preslength,fs.readdirSync(__dirname+ `/cdn/${directory}`).length)
+        var files = fs.readdirSync(__dirname+ `/cdn/${directory}`).sort();
+        var file = files[files.length - 1]
+        try{
+        preslength = Math.max(preslength, Number(path.basename(file, path.extname(file))))
+        } catch (err) {
+            throw new Error("invalid presentation")
+        }
     })
 
     var info = {  
@@ -59,7 +66,8 @@ app.get('/slideinfo/', (req, res) => {
 
 app.get('/slideinfo/:presid/:slide', (req, res) => {
     //console.log(path.extname(findFile(req.params.presid, req.params.slide)));
-    var type = ()=>{
+    var type = ()=>{ 
+        try{
         switch(path.extname(findFile(req.params.presid, req.params.slide))){
             case ".png":
             case ".jpg":
@@ -69,10 +77,17 @@ app.get('/slideinfo/:presid/:slide', (req, res) => {
             case ".mp4":
                 return "video"
                 break;
+            case ".black":
+                return "black"
+                break;
             default:
                 return "img"
                 break;
         }
+        } catch(err){
+            console.log(`[info] "${req.params.presid}"-presentation missing slide ${req.params.slide}`)
+            return "keep";
+        }   
     }
     var info = {
         "type":type(),
@@ -81,13 +96,18 @@ app.get('/slideinfo/:presid/:slide', (req, res) => {
 })
 
 app.get('/slide/:presid/:slide', (req, res) =>{
-    res.sendFile(findFile(req.params.presid, req.params.slide))
+    try {
+        res.sendFile(findFile(req.params.presid, req.params.slide))
+    } catch(err){
+        res.send("Slide not found")
+        console.log(`[info] "${req.params.presid}"-presentation missing slide ${req.params.slide}`)
+    }
 })
 
 function findFile(presid, slide){
     const directoryToSearch = __dirname + `/cdn/${presid}`; // Replace with the directory you want to search in
     const fileNameToSearch = slide; // Replace with the desired file name (without extension)
-    console.log(directoryToSearch)
+    // console.log(directoryToSearch)
     const files = fs.readdirSync(directoryToSearch);
     
     for (const file of files) {
@@ -98,28 +118,52 @@ function findFile(presid, slide){
         return filePath; 
       }   
     }
-    throw Error("File not found")
+    console.log("file not found")
 }
 
 wss.on('connection', (ws) => {
     ws.send(slide)
-    console.log('Websocket connected')
+    // console.log('Websocket connected')
 
     ws.on('message', (message) => {
-        console.log(`Recieved: ${message}`);
-        slide = Number(`${message}`)
-        wss.clients.forEach((client) => {
-            console.log(client)
-            // console.log("i")
-            // if(client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(Number(`${message}`));
-                // console.log(`${message}`);
-                // console.log('send' + message)
-            // }
-        });
+        try {
+            const parsedMessage = JSON.parse(message);
+            const { event, data } = parsedMessage;
+        
+            if (event === 'register') {
+              const clientName = data;
+    
+              // Store the WebSocket connection with the client name
+            //   clients.set(clientName, ws);
+          
+              console.log(`Client ${clientName} connected`);
+              return;
+            }
+            if(event === 'slide'){
+                console.log(`Recieved: ${data}`);
+                slide = Number(`${data}`)
+                wss.clients.forEach((client) => {
+                    // console.log("i")
+                    // if(client !== ws && client.readyState === WebSocket.OPEN) {
+                        client.send(Number(`${data}`));
+                        // console.log(`${message}`);
+                        // console.log('send' + message)
+                    // }
+                });
+            }
+          } catch (error) {
+            console.error('Error parsing message:', error);
+          }
+
     })
-    ws.on('close', (code, reason) => {
-        console.log('WebSocket disconnected');
+        ws.on('close', (code, reason) => {
+        //   const disconnectedClient = Array.from(clients.entries()).find(([name, client]) => client === ws);
+        //   if (disconnectedClient) {
+        //   const clientName = disconnectedClient;
+        //   console.log(disconnectedClient)
+        //   console.log(`Client ${clientName} disconnected`);
+        //   clients.delete(clientName); // Remove the entry
+        // }
     }) 
 })
 
